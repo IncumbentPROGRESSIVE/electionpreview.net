14dc5aab20c825f65a37923b6fd394800a26bc0e
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-06-16 14:27:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,23 @@ let Moderate = true; // Corrected this line
 // Get the input and output elements
 var userInput = document.getElementById('userInput');
 var outputBox = document.getElementById('outputBox');
+var secondOutput = document.getElementById('secondOutput'); // Add this line
 
 // Listen for when the input box's value changes
 userInput.addEventListener('input', function() {
   // Store the user's input in a variable
   var userText = userInput.value;
-  
+
   // Check if the user's text is over 20 characters
   if (userText.length > 20) {
     Moderate = false; // Set Moderate to false
   } else {
     Moderate = true; // Set Moderate to true
   }
-  
+
   // Update the output box with the user's text
   outputBox.value = userText + " test this";
+  
+  // Update the secondOutput box with the user's text
+  secondOutput.value = userText;
 });
