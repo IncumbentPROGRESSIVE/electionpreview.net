fea0ac9248e2c7cfb8aaf21b06f20efe76bf3771
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-06-22 21:14:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,15 @@ toggleButton.addEventListener('click', function() {
   if (electionIframe.src.includes('senate')) {
     electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/?mode=gubernatorial';
     toggleButton.textContent = 'Gubernatorial';
+    caption.textContent = 'Gubernatorial'; // Change the caption text
   } else if (electionIframe.src.includes('gubernatorial')) {
     electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/';
     toggleButton.textContent = 'House';
+     caption.textContent = 'House of Representatives'; // Change the caption text
   } else {
     electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/?mode=senate';
     toggleButton.textContent = 'Senate';
+     caption.textContent = 'Senate'; // Change the caption text
   }
 });
 
