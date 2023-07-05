f07002fd833aa79a9fc51b5475e987e6cde6e3fc
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-07-05 18:02:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,18 +41,6 @@ if (savedUserInput) {
   outputBox.value = savedUserInput + " test this";
 }
 
-darkModeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-  darkModeButton.classList.toggle('on');
-  
-  // Save Dark Mode setting in localStorage
-  if (document.body.classList.contains('dark-mode')) {
-    localStorage.setItem('dark-mode', 'true');
-  } else {
-    localStorage.setItem('dark-mode', 'false');
-  }
-});
-
 window.addEventListener('DOMContentLoaded', (event) => {
   // Check for dark mode preference on page load
   if(localStorage.getItem('dark-mode') === 'true') {
@@ -66,6 +54,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
     userInput.value = savedUserInput;
     outputBox.value = savedUserInput + " test this";
   }
+
+  darkModeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    darkModeButton.classList.toggle('on');
+
+    // Save Dark Mode setting in localStorage
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('dark-mode', 'true');
+    } else {
+      localStorage.setItem('dark-mode', 'false');
+    }
+  });
 });
 
 
