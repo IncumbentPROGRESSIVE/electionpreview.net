4f8ba6679d2bcc55d854de05e56087cb0b5dc714
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-07-05 18:47:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 // Get input and output elements
-var userInput = document.getElementById('userInput');
-var outputBox = document.getElementById('outputBox');
 var toggleButton = document.getElementById('toggleButton');
 var electionIframe = document.getElementById('electionIframe');
 var caption = document.querySelector('.caption');
 var darkModeButton = document.getElementById('darkModeButton');
-
-// Listen for when the input box's value changes
-userInput.addEventListener('input', function() {
-  // Store the user's input in a variable
-  var userText = userInput.value;
-
-  // Store the user's input in localStorage
-  localStorage.setItem('userInput', userText);
-  
-  // Update the output box with the user's text
-  outputBox.value = userText + " test this";
-});
-
+ 
 toggleButton.addEventListener('click', function() {
   if (electionIframe.src.includes('senate')) {
     electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/?mode=gubernatorial';
@@ -34,25 +20,11 @@ toggleButton.addEventListener('click', function() {
   }
 });
 
-// Retrieve the user input from localStorage, if available
-var savedUserInput = localStorage.getItem('userInput');
-if (savedUserInput) {
-  userInput.value = savedUserInput;
-  outputBox.value = savedUserInput + " test this";
-}
-
 window.addEventListener('DOMContentLoaded', (event) => {
   // Check for dark mode preference on page load
   if(localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-mode');
     darkModeButton.classList.add('on');
-  }
-
-  // Retrieve the user input from localStorage, if available
-  var savedUserInput = localStorage.getItem('userInput');
-  if (savedUserInput) {
-    userInput.value = savedUserInput;
-    outputBox.value = savedUserInput + " test this";
   }
 
   darkModeButton.addEventListener('click', () => {
