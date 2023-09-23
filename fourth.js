b83bba6526a558238be00941d53c63ad7c163962
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-09-23 19:17:00 -0400

    Update fourth.js

--- fourth.js
+++ fourth.js
@@ -1,5 +1,6 @@
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="fourthstyle.css">  
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
 google.charts.load('current', {'packages':['corechart']});
